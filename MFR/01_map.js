9e9723e6497c460d5ab9a096528ba52cdff1d6ab
shahidrnc <151998807+shahidrnc@users.noreply.github.com>
2024-01-09 08:52:26 +0000

    MFR Success

---
+++ MFR/01_map.js
@@ -0,0 +1,5 @@
+const mynum=[1,2,3,4,5,6,7,8,9,10]
+const mapnum=mynum
+                .map((num)=> num+10)
+                .map((num)=> num*10)
+console.log(mapnum)
---
+++ MFR/02_filter.js
@@ -0,0 +1,3 @@
+const mynum=[10,20,30,40,50,30,20,60,70,80,100]
+const filternum=mynum.filter((num)=> num<50)
+console.log(filternum);
