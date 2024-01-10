6cf62c01ed0938207bc4fe616d4e0137fe1a0fd9
shahidrnc <151998807+shahidrnc@users.noreply.github.com>
2024-01-10 13:33:46 +0000

    push

---
+++ 01_basics/Variable.js
@@ -0,0 +1,9 @@
+const rollNo=4
+let sname="Shahid"
+var email="Shahidrmc1999@gmail.com"
+mobilenumber=8808613979
+// rollNo=5 const value cannot be change...
+sname="Ansari"
+email="shahidrnc2000@gmail.com"
+mobilenumber=6398313284
+console.table([rollNo,sname,email,mobilenumber])
